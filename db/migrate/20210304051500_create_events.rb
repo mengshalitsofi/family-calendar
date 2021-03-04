98741382ba0f9e1b45d9171818417b7a4b64911b
mengshalitsofi <mengshalitsofi@gmail.com>
2021-03-03 21:19:36 -0800

    set up tables and models

---
+++ db/migrate/20210304051500_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :description
+      t.datetime :event_time
+      t.string :location
+      t.integer :user_id
+    end    
+  end
+end
